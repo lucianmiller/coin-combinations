7dd8e7a88c61cba82bfdaaf95a79e411718ded88
Lucian Miller <lucian.miller185@gmail.com>
2023-11-03 09:22:30 -0700

    Add parameters

--- CoinCombinations/Models/Coins.cs
+++ CoinCombinations/Models/Coins.cs
@@ -4,6 +4,13 @@ namespace CoinCombinations.Models
 {
   public class CoinCombinations
   {
-    
+    public List<string> Coins { get; set; }
+    public List<int> Worth { get; set; }
+
+    public CoinCombinations()
+    {
+      Coins = new List<string> {"penny", "nickle", "dime", "quarter"}
+      Worth = new List<int> {1, 5, 10, 25}
+    }
   }
 }

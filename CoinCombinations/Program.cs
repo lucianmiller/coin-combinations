9e7c70e30521c9e7a44d925d390d3e88ddb189cd
Lucian Miller <lucian.miller185@gmail.com>
2023-11-30 11:21:11 -0800

    Add UI logic

--- CoinCombinations/Program.cs
+++ CoinCombinations/Program.cs
@@ -7,7 +7,15 @@ namespace CoinCombinations
   {
     static void Main()
     {
-      
+      Console.WriteLine("Please enter amount of money you have:");
+      string userInput = Console.ReadLine();
+      CoinCounter coinCombinations = new CoinCounter(userInput);
+      coinCombinations.CountCoins();
+
+      Console.WriteLine("quarters: " + coinCombinations._output["quarter"]);
+      Console.WriteLine("dimes: " + coinCombinations._output["dime"]);
+      Console.WriteLine("nickels: " + coinCombinations._output["nickel"]);
+      Console.WriteLine("pennies: " + coinCombinations._output["penny"]);
     }
   }
 }

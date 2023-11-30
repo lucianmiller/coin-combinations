380ceb6e945c28b1888f2edb3c035004d35b6f27
Lucian Miller <lucian.miller185@gmail.com>
2023-11-30 11:20:57 -0800

    Add function to calculate coin combinations

--- CoinCombinations/Models/Coins.cs
+++ CoinCombinations/Models/Coins.cs
@@ -1,16 +1,60 @@
 using System.Collections.Generic;
+using System;
 
 namespace CoinCombinations.Models
 {
-  public class CoinCombinations
+  public class CoinCounter
   {
-    public List<string> Coins { get; set; }
-    public List<int> Worth { get; set; }
-
-    public CoinCombinations()
+    public Dictionary<string, float> _coins = new Dictionary<string, float>
     {
-      Coins = new List<string> {"penny", "nickle", "dime", "quarter"}
-      Worth = new List<int> {1, 5, 10, 25}
+      {"penny", 0.01f},
+      {"nickel", 0.05f},
+      {"dime", 0.1f},
+      {"quarter", 0.25f}
+    };
+    public Dictionary<string, int> _output = new Dictionary<string, int>
+    {
+      {"penny", 0},
+      {"nickel", 0},
+      {"dime", 0},
+      {"quarter", 0}
+    };
+    public float UserPrice { get; set; }
+
+    public CoinCounter(string userInput)
+    {
+      UserPrice = float.Parse(userInput);
+    }
+
+    public void CountCoins()
+    {
+      while (UserPrice > 0)
+      {
+        if (UserPrice > _coins["quarter"])
+        {
+          int numOfQuarters = Convert.ToInt32(Math.Floor(UserPrice / _coins["quarter"]));
+          _output["quarter"] = numOfQuarters;
+          UserPrice = (float)Math.Round(UserPrice - (numOfQuarters * _coins["quarter"]), 2);
+        }
+        else if (UserPrice > _coins["dime"])
+        {
+          int numOfDimes = Convert.ToInt32(Math.Floor(UserPrice / _coins["dime"]));
+          _output["dime"] = numOfDimes;
+          UserPrice = (float)Math.Round(UserPrice - (numOfDimes * _coins["dime"]), 2);
+        }
+        else if (UserPrice > _coins["nickel"])
+        {
+          int numOfNickels = Convert.ToInt32(Math.Floor(UserPrice / _coins["nickel"]));
+          _output["nickel"] = numOfNickels;
+          UserPrice = (float)Math.Round(UserPrice - (numOfNickels * _coins["nickel"]), 2);
+        }
+        else if (UserPrice > _coins["penny"])
+        {
+          int numOfPennies = Convert.ToInt32(Math.Floor(UserPrice / _coins["penny"]));
+          _output["penny"] = numOfPennies;
+          UserPrice = (float)Math.Round(UserPrice - (numOfPennies * _coins["penny"]), 2);
+        }
+      }
     }
   }
 }
